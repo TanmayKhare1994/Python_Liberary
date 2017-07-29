6a339f717886c81a43853ebe1f915c1fd1c39df5
Akhilesh Kumar <akhileshsh026@users.noreply.github.com>
2017-07-29 18:03:40 +0530

    Strings
    String formatting and string functions .

---
+++ String_Formatting.py
@@ -0,0 +1,7 @@
+num=[12,23,34]
+message = "Numbers are :{0} {1}".format(num[0],num[1])
+print message
+
+#string formatting with the named argument
+
+print "Numbers are :' {x},{y}".format(x=223,y=23)
---
+++ String_Functions.py
@@ -0,0 +1,13 @@
+# join - joins a list of strings with another string as a separator
+
+print   (":".join(["spam","12","jk","adad","23.34"]))
+
+#replace - replaces one substring in a string with another.
+
+print ("Akhilesh kumar Sahu".replace("Sahu","X"))
+
+# startswith and endswith - determine if there is a substring at the start and end of a string, respectively.
+
+print ("this is my ball".startswith("this")) # retun true if condition satisfy ealse false.
+
+print ("this is my ball".endswith("ball"))
