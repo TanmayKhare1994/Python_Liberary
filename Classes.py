c13b744de6ac9cb1ff2da26614a941e7506c9573
Akhilesh Appwala <akhileshsh028@outlook.com>
2017-08-10 12:25:02 +0530

    Claases
    one of the major concept of Oops languages ,
    We have previously looked at two paradigms of programming - imperative (using statements, loops, and functions as subroutines), and functional (using pure functions, higher-order functions, and recursion).
    
    Another very popular paradigm is object-oriented programming (OOP).
    Objects are created using classes, which are actually the focal point of OOP.
    The class describes what the object will be, but is separate from the object itself. In other words, a class can be described as an object's blueprint, description, or definition.
    You can use the same class as a blueprint for creating multiple different objects. 
    
    Classes are created using the keyword class and an indented block, which contains class methods (which are functions). 
    Below is an example of a simple class and its objects.

---
+++ Classes.py
@@ -0,0 +1,11 @@
+class Men:
+    Nums=4;
+    def __init__(self,Name,Bith_Place,Occu):
+        self.Name= Name;
+        self.Birth_place = Bith_Place;
+        self.Occu= Occu;
+
+Fliex = Men("Akhilesh Kumar Sahu","Mandla","Software Engineer");
+
+printa = Men("as","as","jk")
+print printa.Occu,printa.Nums;
