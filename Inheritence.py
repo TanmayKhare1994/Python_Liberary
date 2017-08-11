cc771f3b257ff450e87b9a2accb9f71956064083
Akhilesh Kumar Sahu <Akhileshsh028@outlook.com>
2017-08-11 17:06:07 +0530

    The function super is a useful inheritance-related function that refers to the parent class. It can be used to find the method with a certain name in an object's superclass.

--- Inheritence.py
+++ Inheritence.py
@@ -16,3 +16,38 @@ Cata_obj= Cat("black","4");
 
 print Doga_Obj.color+" "+str(Doga_Obj.legs),Doga_Obj.bark(),"\n";
 print Cata_obj.color + " "+Cata_obj.legs , Cata_obj.Miyon();
+
+print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+print "\t \t Inheritance can also be indirect. One class can inherit from another, and that class can inherit from a third class \t \t ";
+print "\n"
+
+class A:
+    def method(self):
+        print("A method")
+
+
+class B(A):
+    def another_method(self):
+        print("B method")
+
+
+class C(B):
+    def third_method(self):
+        print("C method")
+
+
+c = C()
+c.method()
+c.another_method()
+c.third_method()
+
+
+print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+print "\n"
+print "\t \t function super : \t \t"
+
+class D(A):
+    def method(self):
+        print ("Class D");
+       # super(A, self).method();  # not working in py 3.0 it is super.method();
