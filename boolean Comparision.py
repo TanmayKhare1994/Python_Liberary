50707469ae0b2b35a17a52c439a4a02c0cd4d8c1
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-09-02 17:16:45 +0530

    Python version changed

--- boolean Comparision.py
+++ boolean Comparision.py
@@ -1,23 +1,25 @@
+from __builtin__ import raw_input
+
 my_bolean= True
 
-print my_bolean
+print (my_bolean)
 
-print 45!=65
+print (45!=65)
 
 # ifelse
 
 inn = int(raw_input("Enter number"))
 if(inn<5):
-    print "Numer is less than 5 :"+str(inn)
+    print ("Numer is less than 5 :"+str(inn))
 elif(inn>100):
-    print "Number is grater than 100"
+    print ("Number is grater than 100")
 else:
-    print "Please enter the number"
+    print ("Please enter the number")
 
 # opertion 2
 if not True:
-    print "1"
+    print ("1")
 elif not(1+1==3):
-    print "2"
+    print ("2")
 else:
-    print "3"
+    print ("3")
