1506dfd1494dceaeed1f19cad7b5b84cf136a610
Akhilesh Kumar Sahu <Akhileshsh028@outlook.com>
2017-09-08 11:39:52 +0530

    In this example , the match function did not match the pattern, as it looks at the beginning of the string. The search function found a match in the string. The function re.finditer does the same thing as re.findall, except it returns an iterator, rather than a list.

--- INtoduction.py
+++ INtoduction.py
@@ -1,8 +1,11 @@
 import  re
 
-pattern = r"Akhilesh"
+#only the word starts from starting it will take example below
+pattern = r"sp" #it will accept
+pattern1= r"spam" #it will accept
+pattern2 = r"pamspam" #it will not accept becuase word starting from p.
 
-if(re.match(pattern,"AkhileshKumarSahu AkhileshKumarSahu")):
+if(re.match(pattern,"spamspamspam")):
     print ("MAtched")
 else:
     print ("Not Matched")
