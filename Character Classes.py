4ec6938aad02f7f8d210555c43c54199c6b1636d
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-09-12 07:56:59 +0530

    pattern 3 is all about invert should be in front of the charcter classes other wise no sense of it .

--- Character Classes.py
+++ Character Classes.py
@@ -11,9 +11,25 @@ if re.search(pattern,"Myth"):
 #ex2
 pattern1 = r"[A-Z][A-Z][0-9]"       # check 1st ltter by 1st block followed others letterby the numbers.
 pattern2 = r"[A-Za-z][0-9]"
+pattern3 = r"[^A-Z]"
 
 if re.search(pattern1,"LS8"):
-    print("MAtch email")
+    print("MAtch email");
 
 if re.search(pattern2,"Akhileshsh026"):
-    print("Correct USername")
+    print("Correct USername");
+
+if re.search(pattern3,"9087"):
+    print("pattern 3 matched");
+
+
+if re.search(pattern3, "this is all quiet"):  #except capital lettrs there are other cases too
+    print("pattern 3 matched");
+
+if re.search(pattern3, "AbCdEfG123"):           #mixer
+    print("pattern 3 matched");
+
+if re.search(pattern3, "THISISALLSHOUTING"): #all cpas not allowed
+    print("pattern 3 matched");
+else:
+    print("pattern not  3 matched")
