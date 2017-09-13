9a3e059be03673d6c7897f4e75354f19bb000978
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-09-13 23:57:09 +0530

    Default Values
    Named parameters to a function can be made optional by giving them a default value.
    These must come after named parameters without a default value.

--- Function_params_parmeter_equivalent.py
+++ Function_params_parmeter_equivalent.py
@@ -5,3 +5,10 @@ def function(named_arg, *args):
 
 function(1, 2, 3, 4, 5)
 
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+def function(x, y, food="spam"):
+   print(food)
+
+function(1, 2)
+function(3, 4, "egg")
