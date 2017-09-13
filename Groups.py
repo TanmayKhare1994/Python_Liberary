abc00400558bdff8e3fb0e287105d192c1a8c9b0
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-09-13 18:30:30 +0530

    There are several kinds of special groups. Two useful ones are named groups and non-capturing groups. Named groups have the format (?P<name>...), where name is the name of the group, and ... is the content. They behave exactly the same as normal groups, except they can be accessed by group(name) in addition to its number. Non-capturing groups have the format (?:...). They are not accessible by the group method, so they can be added to an existing regular expression without breaking the numbering.

--- Groups.py
+++ Groups.py
@@ -21,12 +21,34 @@ if re.search(pattern2,"bab"):
 
 import re
 
-pattern = r"a(bc)(de)(f(g)h)i"
+pattern3 = r"a(bc)(de)(f(g)h)i"
+pattern4 = r"(a-z,A-Z)(@)(a-z)(.)(a-z)"
 
-match = re.match(pattern, "abcdefghijklmnop")
+match = re.match(pattern3, "abcdefghijklmnop")
 if match:
    print(match.group()) # returns the whole match
    print(match.group(0)) # match only 1st group
    print(match.group(1)) # matches only 2nd group
    print(match.group(2)) # mathes only 3rd group
    print(match.groups()) # matches all groups content from 1
+
+match1 = re.match(pattern4,"akhileshsh@hotmail.com")  # not done
+if match1:
+   print(match1.group()) # returns the whole match
+   print(match1.group(0)) # match only 1st group
+   print(match1.group(1)) # matches only 2nd group
+   print(match1.group(2))
+
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+# 1st group is named group and 2nd group is non capturing group
+pattern = r"(?P<first>abc)(?:def)(ghi)"
+
+match = re.match(pattern, "abcdefghi")
+if match:
+   print(match.group("first"))
+   print(match.groups())
+
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+
