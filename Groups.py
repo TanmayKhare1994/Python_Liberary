987de61ade4aa3b6df80545647364e00f07f7b75
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-09-13 18:14:04 +0530

    Groups :
    The content of groups in a match can be accessed using the group function.
    A call of group(0) or group() returns the whole match.
    A call of group(n), where n is greater than 0, returns the nth group from the left.
    The method groups() returns all groups up from 1.

--- Groups.py
+++ Groups.py
@@ -16,3 +16,17 @@ pattern2 = '([^aeiou][aeiou][^aeiou])+'
 
 if re.search(pattern2,"bab"):
     print("Sucess")
+
+#groups
+
+import re
+
+pattern = r"a(bc)(de)(f(g)h)i"
+
+match = re.match(pattern, "abcdefghijklmnop")
+if match:
+   print(match.group()) # returns the whole match
+   print(match.group(0)) # match only 1st group
+   print(match.group(1)) # matches only 2nd group
+   print(match.group(2)) # mathes only 3rd group
+   print(match.groups()) # matches all groups content from 1
