35baa5ddeeb40d42c2b2daa674de208902468c8a
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-09-13 18:07:42 +0530

    example to undersand

--- Groups.py
+++ Groups.py
@@ -11,4 +11,8 @@ if re.search(pattern,"Akhilesh Kumar Kumar Kumar Kumar"):
 if re.search(pattern,"Kumar"):
     print("saved 2")
 
+#example ^ excluding any of the vowel inside the bracket
+pattern2 = '([^aeiou][aeiou][^aeiou])+'
 
+if re.search(pattern2,"bab"):
+    print("Sucess")
