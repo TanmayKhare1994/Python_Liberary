fd4370856c4950a2b40d03755f7f932bda145af5
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-09-14 13:50:01 +0530

    carefully watch the example

--- Touple Unpacking/Swaping the Numbers.py
+++ Touple Unpacking/Swaping the Numbers.py
@@ -10,3 +10,20 @@ a, b = b, a
 print(a)
 print(b)
 print(c)
+
+print("+++++++++++++")
+
+a, b, *c, d = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(a)
+print(b)
+print(c)
+print(d)
+
+print("++++++++++++++++++++++++++++++++++++++")
+
+a, b, *c,d,e = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
