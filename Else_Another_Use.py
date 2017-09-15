3b851793ea74c36680534ddce3a714d0976e0f9e
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-09-15 23:01:58 +0530

    The else statement is most commonly used along with the if statement, but it can also follow a for or while loop, which gives it a different meaning. With the for or while loop, the code within it is called if the loop finishes normally (when a break statement does not cause an exit from the loop).

---
+++ Else_Another_Use.py
@@ -0,0 +1,13 @@
+for i in range(10):
+   if i == 15:
+      break
+else:
+   print("Unbroken 1")
+
+for i in range(10):
+   if i == 5:
+      break
+else:
+   print("Unbroken 2")
+
+# else executre after the loop finishs the task sucessfully.
