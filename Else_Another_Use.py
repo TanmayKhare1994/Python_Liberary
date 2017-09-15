5a54daa68edaf9bdc87ead54dd537ca0457c45ab
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-09-15 23:09:54 +0530

    The else statement can also be used with try/except statements. In this case, the code within it is only executed if no error occurs in the try statement

--- Else_Another_Use.py
+++ Else_Another_Use.py
@@ -11,3 +11,26 @@ else:
    print("Unbroken 2")
 
 # else executre after the loop finishs the task sucessfully.
+
+for i in range(10):
+   if i > 5:
+      print(i)
+      break
+else:
+   print("7")
+
+# with try execute block
+
+try:
+   print(1)
+except ZeroDivisionError:
+   print(2)
+else:
+   print(3)
+
+try:
+   print(1/0)
+except ZeroDivisionError:
+   print(4)
+else:
+   print(5)
