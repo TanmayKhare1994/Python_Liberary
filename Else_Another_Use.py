2cbf8cdc8b9ccb2d02bb4f65b203d6a34774fd9d
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-09-16 17:25:19 +0530

    Final

--- Else_Another_Use.py
+++ Else_Another_Use.py
@@ -34,3 +34,15 @@ except ZeroDivisionError:
    print(4)
 else:
    print(5)
+
+# example
+try:
+  print(1)
+  print(1 + "1" == 2)
+  print(2)
+except TypeError:
+  print(3)
+else:
+  print(4)
+
+ 
--- Scripts.py
+++ Scripts.py
@@ -3,4 +3,19 @@ def func():
 
 if __name__=="__main__":
     print("This is script")
-    
+
+#
+# Most Python code is either a module to be imported, or a script that does something.
+# However, sometimes it is useful to make a file that can be both imported as a module and run as a script.
+# To do this, place script code inside if __name__ == "__main__".
+# This ensures that it won't be run if the file is imported.
+# ! note :- try to run in console then name will run but if you import this in your project then the function will run .
+
+for i in range(10):
+  try:
+    if 10 / i == 2.0:
+      break
+  except ZeroDivisionError:
+    print(1)
+  else:
+    print(2)
