44b33489e96a6e73f5fa4863c892c3db7d86bf27
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-09-16 19:17:52 +0530

    Python uses words for its Boolean operators, whereas most other languages use symbols such as &&, || and !.

--- Practise/BAsics.py
+++ Practise/BAsics.py
@@ -1,2 +1,22 @@
 for r in range(6):
     print(r, "hello")
+
+print (10//5)
+
+str = "Biran is in \t the'e \n gaurage"
+
+print (str)
+
+print("++++++++++++++++++++++++++++++++++++++")
+
+print("str"!="jk")
+
+# Python uses words for its Boolean operators, whereas most other languages use symbols such as &&, || and !.
+
+print(1 == 1 and 2 == 2)
+
+print(1 == 1 and 2 == 3)
+
+print(1 != 1 and 2 == 2)
+
+print(2 < 1 and 3 >  6)
